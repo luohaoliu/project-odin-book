f13c049e561be3a19d896a9aa5c701bc1b497ab8
luohaoliu <liuluohao@gmail.com>
2021-10-16 19:58:54 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var dotenv = require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require('cors');
+
+require('./mongoose');
+require('./passport');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
+var loginRouter = require('./routes/login');
+var logoutRouter = require('./routes/logout');
+var signupRouter = require('./routes/signup');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(cors({
+  origin: ['https://localhost:3000']
+}));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+app.use('/signup', signupRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  first_name: {type: String},
+  last_name: {type: String},
+  username: {type: String},
+  password: {type: String},
+  status: {type: String},
+  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  friend_requests: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  pictures: "",
+});
+
+UserSchema.virtual("url").get(function () {
+  return "/user/" + this._id;
+});
+
+module.exports = mongoose.model("User", UserSchema);
